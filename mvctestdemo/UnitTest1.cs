cfa362e536562d373d1194593a9378d58ed23d10
PRAHA <PRAHA@DESKTOP-7P3CUD8>
2022-10-11 19:11:03 +0530

    Add project files.

---
+++ mvctestdemo/UnitTest1.cs
@@ -0,0 +1,15 @@
+using mvcdemo;
+
+namespace mvctestdemo
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(0, 0);
+                
+        }
+    }
+}
